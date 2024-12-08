cf9cebfcffd941888ba95f9955cf140bf2a7324c
Miko Mynttinen <mikokm@gmail.com>
2024-12-08 04:23:01 +0100

    day 6.2 and 7

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -23,6 +23,34 @@ fn in_graph(g: &Graph, x: i32, y: i32) -> bool {
     true
 }
 
+fn solve(graph: &mut Graph) -> (HashSet<(i32, i32)>, bool) {
+    let xs = find_char(&graph, '^');
+    let directions = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
+    let mut pos = xs[0];
+    let mut dir_index = 0;
+    let mut unique_positions = HashSet::from([pos]);
+    let mut previous_edges: HashSet<((i32, i32), (i32, i32))> = HashSet::new();
+
+    loop {
+        let (oob, last_pos, positions) = walk_forward(graph, pos, directions[dir_index]);
+        for position in &positions {
+            unique_positions.insert(*position);
+        }
+
+        if oob {
+            return (unique_positions, false);
+        }
+
+        if !previous_edges.insert((pos, last_pos)) {
+            println!("Cycle detected!");
+            return (unique_positions, true);
+        }
+
+        pos = last_pos;
+        dir_index = (dir_index + 1) % directions.len();
+    }
+}
+
 fn main() {
     let _file = read_to_string("input.txt").unwrap();
     let input = r"....#.....
@@ -49,32 +77,30 @@ fn main() {
     };
 
     print_graph(&graph);
+    let mut cycle_pos = vec![];
 
-    let xs = find_char(&graph, '^');
-    let directions = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
-    let mut pos = xs[0];
-    let mut dir_index = 0;
-    let mut unique_positions = HashSet::from([pos]);
+    for y in 0..=graph.len() - 1 {
+        for x in 0..=graph[0].len() - 1 {
+            if graph[y][x] == '#' || graph[y][x] == '^' {
+                continue;
+            }
 
-    loop {
-        let (oob, last_pos, positions) = walk_forward(&mut graph, pos, directions[dir_index]);
-        for position in &positions {
-            unique_positions.insert(*position);
+            let mut new_graph = graph.clone();
+            new_graph[y][x] = '#';
+            let (_, cycles) = solve(&mut new_graph);
+            // print_graph(&new_graph);
+            // println!("{:?} : {}", (x, y), cycles);
+            if cycles {
+                cycle_pos.push((x, y));
+            }
         }
-
-        if oob {
-            break;
-        }
-
-        pos = last_pos;
-        dir_index = (dir_index + 1) % directions.len();
     }
 
-    for (x, y) in &unique_positions {
-        assert!(in_graph(&graph, *x, *y));
-        graph[*y as usize][*x as usize] = 'X';
+    println!("cycles: {:?}, len: {}", cycle_pos, cycle_pos.len());
+    for (x, y) in cycle_pos {
+        graph[y][x] = 'O';
     }
-    println!("Result: {}", unique_positions.len());
+
     print_graph(&graph);
 }
 
@@ -83,23 +109,23 @@ fn walk_forward(
     start_pos: (i32, i32),
     delta: (i32, i32),
 ) -> (bool, (i32, i32), Vec<(i32, i32)>) {
-    println!("Walking dir = {:?}, start_pos = {:?}", &delta, &start_pos);
+    // println!("Walking dir = {:?}, start_pos = {:?}", &delta, &start_pos);
     let mut positions = Vec::new();
     let mut pos = start_pos;
 
     loop {
         let next = (pos.0 + delta.0, pos.1 + delta.1);
-        println!("Next {:?}", &next);
+        // println!("Next {:?}", &next);
         if !in_graph(&g, next.0, next.1) {
-            println!("oob {:?}", &pos);
+            // println!("oob {:?}", &pos);
             return (true, pos, positions);
         }
 
         let c = g[next.1 as usize][next.0 as usize];
-        println!("Visited {:?} ({})", &pos, c);
+        // println!("Visited {:?} ({})", &pos, c);
 
         if c == '#' {
-            println!("Hit wall");
+            // println!("Hit wall");
             return (false, pos, positions);
         }
 
