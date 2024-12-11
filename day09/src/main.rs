6cd100060a6645a26d0a013824e4c4c8a3469ad7
Miko Mynttinen <mikokm@gmail.com>
2024-12-11 14:13:08 +0100

    Day 9.2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::read_to_string;
 
 fn main() {
     let _file = read_to_string("input.txt").unwrap();
-    // let input = r"2333133121414131402";
+    let input = r"2333133121414131402";
     // let input = r"12345";
     let input = _file;
     // println!("{}", input);
@@ -26,7 +26,10 @@ fn main() {
         }
     }
 
+    println!("sizes: {:?}, spaces: {:?}", sizes, spaces);
+
     let mut packed = unpacked.clone();
+    /* part 1
     for i in 0..packed.len() - 1 {
         // println!("packed[{:?}] = {:?}", i, packed[i]);
         if packed[i] == -1 {
@@ -40,18 +43,71 @@ fn main() {
         }
         // println!("packed: {:?}", packed);
     }
+    */
+
+    let next_free = |v: &Vec<i32>, start: usize| -> usize {
+        let mut end = start;
+        // println!("Checking for v[{}] ({})", start, v[start]);
+        for i in start..=v.len() - 1 {
+            end = i;
+
+            if v[i] != -1 {
+                break;
+            }
+        }
+
+        // println!("end {} - start {} = {}", end, start, end - start);
+        return end - start;
+    };
+
+    let write_next = |v: &mut Vec<i32>, start: usize, size: i32, id: i32| {
+        for i in start..start + size as usize {
+            // println!("v[{:?}] = {}", i, id);
+            v[i] = id;
+        }
+    };
+
+    let erase = |v: &mut Vec<i32>, id: usize| {
+        for i in 0..=v.len() - 1 {
+            if v[i] == id as i32 {
+                // println!("v[{:?}] = {}", i, -1);
+                v[i] = -1;
+            }
+        }
+    };
+
+    let files: Vec<(usize, i32)> = sizes.iter().map(|x| *x as i32).enumerate().rev().collect();
+    println!("{:?}", files);
+
+    // Yeah this will take ages.
+    for file in files {
+        // println!("Moving file {:?}", file);
+        let file_start = packed.iter().position(|c| *c == file.0 as i32).unwrap();
+        for i in 0..=unpacked.len() - 1 {
+            let free = next_free(&packed, i) as i32;
+            // println!("Free space {:?}", free);
+            if free >= file.1 && file_start > i {
+                erase(&mut packed, file.0);
+                write_next(&mut packed, i, file.1, file.0 as i32);
+                break;
+            }
+        }
+
+        // println!("{:?}", packed);
+    }
 
     let mut checksum = 0;
     for (i, e) in packed.iter().enumerate() {
         if *e == -1 {
-            println!("exit at {}", i);
-            break;
+            // println!("exit at {}", i);
+            // break;
+            continue;
         }
         checksum += (*e as i64) * i as i64;
     }
 
-    println!("sizes: {:?}, free_space: {:?}", sizes, spaces);
-    println!("unpacked: {:?}", unpacked);
-    println!("packed: {:?}", packed);
+    // println!("sizes: {:?}, free_space: {:?}", sizes, spaces);
+    println!("u: {:?}", unpacked);
+    println!("p: {:?}", packed);
     println!("checksum: {:?}", checksum);
 }
